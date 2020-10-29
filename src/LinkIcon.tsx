058a57ac607d9c4fdbfb86ae44291d475afa87a6
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-29 23:21:11 +0400

    feat: amp

--- src/LinkIcon.tsx
+++ src/LinkIcon.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Image from "next/image";
+import { useAmp } from "next/amp";
 
 export interface ILink {
   linkHref: string;
   imgSrc: string;
   altText: string;
+  size: number | string;
 }
 
 export const LinkIcon: React.FC<ILink> = (props) => {
-  const iconSize = 25;
+  const isAmp = useAmp();
 
   return (
     <a href={props.linkHref} target="_blank" rel="noopener noreferrer">
-      <Image src={props.imgSrc} alt={props.altText} height={iconSize} width={iconSize} />
+      {isAmp ? (
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        <amp-img height={props.size} width={props.size} src={props.imgSrc} alt={props.altText} layout="responsive" />
+      ) : (
+        <Image src={props.imgSrc} alt={props.altText} height={props.size} width={props.size} />
+      )}
     </a>
   );
 };
