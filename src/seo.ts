f64f109792277e73ddf2607e4b4e15f0a8b4682b
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-02 16:32:01 +0400

    feat(seo.ts)

--- src/seo.ts
+++ src/seo.ts
@@ -1,9 +1,5 @@
 export const title = "Ilya Konstantinov Samara Web Developer";
 export const description = "Ilya Konstantinov Samara Web Developer";
-export const keywords: string = new Array<string>(
-  "VueJs",
-  "NodeJs",
-  "Web",
-  "Developer",
-  "Samara"
-).join(", ");
+export const keywords: string = new Array<string>("ReactJs", "VueJs", "NodeJs", "Web", "Developer", "Samara").join(
+  ", ",
+);
