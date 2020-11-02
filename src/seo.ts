f052e5c3aaa44e223e0fa28522e200c7fedc2395
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-02 16:36:45 +0400

    feat(seo.ts): add PWA keyword

--- src/seo.ts
+++ src/seo.ts
@@ -1,5 +1,11 @@
 export const title = "Ilya Konstantinov Samara Web Developer";
 export const description = "Ilya Konstantinov Samara Web Developer";
-export const keywords: string = new Array<string>("ReactJs", "VueJs", "NodeJs", "Web", "Developer", "Samara").join(
-  ", ",
-);
+export const keywords: string = new Array<string>(
+  "PWA",
+  "ReactJs",
+  "VueJs",
+  "NodeJs",
+  "Web",
+  "Developer",
+  "Samara",
+).join(", ");
