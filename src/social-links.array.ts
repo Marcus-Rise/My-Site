23dcc0a048369023b9ea5e0838a0005f3b34cc54
Marcus-Rise <ilykonst95@gmail.com>
2021-08-17 10:20:02 +0400

    feat: stackoverflow link instead of twitter

--- src/social-links.array.ts
+++ src/social-links.array.ts
@@ -2,7 +2,7 @@ import telegram from "../public/icons/telegram.svg";
 import gitlab from "../public/icons/gitlab.svg";
 import github from "../public/icons/github.svg";
 import npmjs from "../public/icons/npmjs.png";
-import twitter from "../public/icons/twitter.png";
+import stackoverflow from "../public/icons/stackoverflow.png";
 
 const socialLinks: ReadonlyArray<{
   linkHref: string;
@@ -14,13 +14,13 @@ const socialLinks: ReadonlyArray<{
     linkHref: "https://t.me/MarcusRise",
     imgSrc: telegram,
     altText: "telegram",
-    size: 20,
+    size: 23,
   },
   {
     linkHref: "https://gitlab.com/marcus_rise",
     imgSrc: gitlab,
     altText: "gitlab",
-    size: 20,
+    size: 25,
   },
   {
     linkHref: "https://github.com/Marcus-Rise",
@@ -29,16 +29,16 @@ const socialLinks: ReadonlyArray<{
     size: 35,
   },
   {
+    linkHref: "https://stackoverflow.com/users/story/8028101",
+    imgSrc: stackoverflow,
+    altText: "stackoverflow",
+    size: 35,
+  },
+  {
     linkHref: "https://www.npmjs.com/~marcus-rise",
     imgSrc: npmjs,
     altText: "npmjs",
-    size: 17,
-  },
-  {
-    linkHref: "https://twitter.com/ilykonst95",
-    imgSrc: twitter,
-    altText: "twitter",
-    size: 25,
+    size: 23,
   },
 ];
 
