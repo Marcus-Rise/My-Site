903e133798e0000f91ca727f8ee484073b54a795
Ilya <ilykonst95@gmail.com>
2021-08-17 10:27:09 +0400

    Stackoverflow (#20)
    * refactor
    
    * feat: stackoverflow link instead of twitter
    
    * feat: telegram icon size
    
    * feat: telegram icon size

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,8 @@
 import type { FC } from "react";
 import React from "react";
 import type { AppProps } from "next/app";
-import Head from "next/head";
-import { description, keywords, title } from "../src/seo";
 import "../src/styles/global.scss";
-import { Layout } from "../src/components";
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="title" content={title} />
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
-    </Head>
-    <Layout center>
-      <Component {...pageProps} />
-    </Layout>
-  </>
-);
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,43 @@
 import type { FC } from "react";
 import React from "react";
-import { description, title } from "../src/seo";
+import { description, keywords, title } from "../src/seo";
 import { Profile } from "../src/profile";
+import Head from "next/head";
+import { Layout } from "../src/components";
+import type { GetStaticProps } from "next";
 
 const config = { amp: "hybrid" };
 
-const Home: FC = () => <Profile title={title} description={description} />;
+interface IHomeProps {
+  title: string;
+  description: string;
+  keywords: string;
+}
+
+const getStaticProps: GetStaticProps<IHomeProps> = () => {
+  return {
+    props: {
+      title,
+      description,
+      keywords,
+    },
+  };
+};
+
+const Home: FC<IHomeProps> = ({ title, description, keywords }) => (
+  <>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+    </Head>
+    <Layout center>
+      <Profile title={title} description={description} />
+    </Layout>
+  </>
+);
 
 export default Home;
-export { config };
+export { config, getStaticProps };
