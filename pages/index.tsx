6153d42da151c42832320904e3a76ec0cd44ebfc
Ilya Konstantinov <ilykonst95@gmail.com>
2021-08-20 14:45:05 +0400

    Merge pull request #21
    * feat: new avatar and icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,6 +32,10 @@ const Home: FC<IHomeProps> = ({ title, description, keywords }) => (
       <meta name="title" content={title} />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
     <Layout center>
       <Profile title={title} description={description} />
