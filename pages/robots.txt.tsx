37fdcc0265eda3a4813de9604fcb2302c675e6ec
Marcus-Rise <ilykonst95@gmail.com>
2021-08-22 20:22:42 +0400

    seo

---
+++ pages/robots.txt.tsx
@@ -0,0 +1,44 @@
+import type { GetServerSideProps } from "next";
+
+const getRobotsStr = (hostName: string): string => {
+  const isAllow = Boolean(process.env.ALLOW_ROBOTS);
+
+  const robots = isAllow
+    ? {
+        "User-agent": "*",
+        Allow: "/",
+        Sitemap: `https://${hostName}/sitemap.xml`,
+      }
+    : {
+        "User-agent": "*",
+        Disallow: "/",
+        Sitemap: `https://${hostName}/sitemap.xml`,
+      };
+
+  return Object.keys(robots)
+    .map((i) => {
+      // @ts-ignore
+      return `${i}: ${robots[i]}`;
+    })
+    .join("\n");
+};
+
+const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const host = String(req.headers.host);
+  const robots = getRobotsStr(host);
+  res.setHeader("Content-Type", "text/plain");
+  res.end(robots);
+
+  return {
+    props: {},
+  };
+};
+
+const Robots = () => {};
+
+export default Robots;
+export { getServerSideProps };
+/*
+This XML file does not appear to have any style information associated with it. The document tree is shown below.
+
+ */
