8ff68aa97131d8d92535e073704e92d34cae2e74
Marcus-Rise <ilykonst95@gmail.com>
2021-08-23 08:54:38 +0400

    layout

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,19 +1,46 @@
 import type { FC } from "react";
-import styles from "./layout.module.scss";
-import classNames from "classnames";
+import { useCallback, useState } from "react";
+import { darkTheme, defaultTheme, GlobalStyles } from "../../styles";
+import styled, { css, ThemeProvider } from "styled-components";
 
 interface ILayoutProps {
   center?: boolean;
 }
 
-const Layout: FC<ILayoutProps> = ({ center, children }) => (
-  <main
-    className={classNames(styles.root, {
-      [styles.center]: center,
-    })}
-  >
-    {children}
-  </main>
-);
+const Main = styled.main<{
+  center?: boolean;
+}>`
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.backgroundTheme};
+
+  ${({ center }) =>
+    !!center &&
+    css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `}
+`;
+
+const Layout: FC<ILayoutProps> = ({ center, children }) => {
+  const [theme, setTheme] = useState(defaultTheme);
+
+  const toggleTheme = useCallback(() => {
+    if (theme === defaultTheme) {
+      setTheme(darkTheme);
+    } else {
+      setTheme(defaultTheme);
+    }
+  }, [theme]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <button onClick={toggleTheme}>toggle theme</button>
+      <Main center={center}>{children}</Main>
+    </ThemeProvider>
+  );
+};
 
 export { Layout };
