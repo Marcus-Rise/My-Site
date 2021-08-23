003d59da4f6f5d2aa92a1e75898f2ff7cfef9e8f
Ilya Konstantinov <ilykonst95@gmail.com>
2021-08-23 12:35:59 +0400

    Theme title (#27)

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,9 +1,7 @@
 import type { FC } from "react";
-import { useMemo } from "react";
 import { GlobalStyles } from "../../styles";
-import styled, { css, ThemeProvider } from "styled-components";
-import { ThemePreferencesEnum, useTheme } from "./theme.hook";
-import dynamic from "next/dynamic";
+import styled, { css } from "styled-components";
+import { Theme } from "../theme";
 
 interface ILayoutProps {
   center?: boolean;
@@ -25,54 +23,11 @@ const Main = styled.main<{
     `}
 `;
 
-const ThemeToggle = styled.button`
-  background-color: ${({ theme }) => theme.backgroundSecondary};
-  border: none;
-  border-radius: 100%;
-  position: fixed;
-  top: 1.5rem;
-  right: 1.5rem;
-  font-size: 1rem;
-  padding: 0.25rem;
-  width: 2.5rem;
-  height: 2.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
-  color: ${({ theme }) => theme.textPrimary};
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const ThemeToggleProvider = dynamic(async () => ThemeToggle, { ssr: false });
-
-const Layout: FC<ILayoutProps> = ({ center, children }) => {
-  const { theme, preferences, toggleTheme } = useTheme();
-
-  const preferencesIcon = useMemo(() => {
-    switch (preferences) {
-      case ThemePreferencesEnum.LIGHT: {
-        return "☀︎";
-      }
-      case ThemePreferencesEnum.DARK: {
-        return "☾";
-      }
-      case ThemePreferencesEnum.SYSTEM: {
-        return "⌽";
-      }
-    }
-  }, [preferences]);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <ThemeToggleProvider onClick={toggleTheme}>{preferencesIcon}</ThemeToggleProvider>
-      <Main center={center}>{children}</Main>
-    </ThemeProvider>
-  );
-};
+const Layout: FC<ILayoutProps> = ({ center, children }) => (
+  <Theme>
+    <GlobalStyles />
+    <Main center={center}>{children}</Main>
+  </Theme>
+);
 
 export { Layout };
---
+++ src/components/theme/theme.component.tsx
@@ -0,0 +1,66 @@
+import dynamic from "next/dynamic";
+import styled, { ThemeProvider } from "styled-components";
+import type { FC } from "react";
+import { useMemo } from "react";
+import { ThemePreferencesEnum, useTheme } from "./theme.hook";
+
+const ThemeToggle = styled.button`
+  background-color: ${({ theme }) => theme.backgroundSecondary};
+  border: none;
+  border-radius: 100%;
+  position: fixed;
+  top: 1.5rem;
+  right: 1.5rem;
+  font-size: 1rem;
+  padding: 0.25rem;
+  width: 2.5rem;
+  height: 2.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  color: ${({ theme }) => theme.textPrimary};
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ThemeToggleProvider = dynamic(async () => ThemeToggle, { ssr: false });
+
+const Theme: FC = ({ children }) => {
+  const { theme, preferences, toggleTheme } = useTheme();
+
+  const { icon, title } = useMemo(() => {
+    let meta: { icon: string; title: string };
+
+    switch (preferences) {
+      case ThemePreferencesEnum.LIGHT: {
+        meta = { title: "Light", icon: "☀︎" };
+        break;
+      }
+      case ThemePreferencesEnum.DARK: {
+        meta = { title: "Dark", icon: "☾" };
+        break;
+      }
+      case ThemePreferencesEnum.SYSTEM:
+      default: {
+        meta = { title: "System", icon: "⌽" };
+        break;
+      }
+    }
+
+    return meta;
+  }, [preferences]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ThemeToggleProvider onClick={toggleTheme} title={title}>
+        {icon}
+      </ThemeToggleProvider>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export { Theme };
