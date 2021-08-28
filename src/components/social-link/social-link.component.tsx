2426c0de4bb6264fb15bd9bab919caf0e043194b
Ilya Konstantinov <ilykonst95@gmail.com>
2021-08-28 16:39:49 +0400

    Habr link (#30)

--- src/components/social-link/social-link.component.tsx
+++ src/components/social-link/social-link.component.tsx
@@ -1,28 +1,19 @@
 import type { FC } from "react";
 import React from "react";
-import { useAmp } from "next/amp";
 import Image from "next/image";
 
 interface ISocialLinkProps {
   linkHref: string;
-  imgSrc: string;
+  imgSrc: string | StaticImageData;
   altText: string;
   size: number | string;
   className?: string;
 }
 
-const SocialLink: FC<ISocialLinkProps> = ({ className, linkHref, size, imgSrc, altText }) => {
-  const isAmp = useAmp();
-
-  return (
-    <a className={className} href={linkHref} target="_blank" rel="noopener noreferrer">
-      {isAmp ? (
-        <amp-img height={size} width={size} src={imgSrc} alt={altText} layout={"fixed"} />
-      ) : (
-        <Image src={imgSrc} alt={altText} height={size} width={size} quality={25} />
-      )}
-    </a>
-  );
-};
+const SocialLink: FC<ISocialLinkProps> = ({ className, linkHref, size, imgSrc, altText }) => (
+  <a className={className} href={linkHref} target="_blank" rel="noopener noreferrer" title={altText}>
+    <Image src={imgSrc} alt={altText} height={size} width={size} quality={25} />
+  </a>
+);
 
 export { SocialLink };
