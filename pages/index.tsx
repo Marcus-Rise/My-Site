7feb2af40f4ab01a2aa62e2b176e77ba021b5685
Marcus-Rise <ilykonst95@gmail.com>
2021-09-01 13:01:20 +0400

    feat: seo image

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ const Home: FC = () => (
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
+      <meta property="og:image" content={"/img/profile.jpg"} />
     </Head>
     <Layout center>
       <Profile title={title} description={description} />
