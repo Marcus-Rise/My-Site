aad780f92c87b44e817ba6db8a075ad1f1fa3e00
Marcus-Rise <ilykonst95@gmail.com>
2022-01-20 14:28:37 +0400

    feat: russian key-words

--- src/seo.ts
+++ src/seo.ts
@@ -2,6 +2,12 @@ import { skills } from "./skills.array";
 
 const title = "Ilya Konstantinov";
 const description = "Samara Web Frontend Developer";
-const keywords: string = [...skills, ...title.split(" "), ...description.split(" ")].join(", ");
+const keywords: string = [
+  ...skills,
+  ...title.split(" "),
+  ...description.split(" "),
+  "Илья",
+  "Константинов",
+].join(", ");
 
 export { title, description, keywords };
