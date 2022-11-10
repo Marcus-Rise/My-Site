aec099023cffa8dbf1b6f8411c623869d794db86
Marcus-Rise <ilykonst95@gmail.com>
2022-11-10 15:27:11 +0400

    feat: detect theme on server side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Roboto } from "@next/font/google";
 import classNames from "classnames";
 import "../styles/global.scss";
 import { Theme } from "../components/theme";
+import { headers } from "next/headers";
 
 const roboto = Roboto({
   weight: "400",
@@ -12,12 +13,18 @@ const roboto = Roboto({
   display: "swap",
 });
 
-const RootLayout: FC<PropsWithChildren> = ({ children }) => (
-  <html lang={"ru"} className={styles.html}>
-    <body className={classNames(styles.body, roboto.variable)}>
-      <Theme />
-      <main className={styles.main}>{children}</main>
-    </body>
-  </html>
-);
+const RootLayout: FC<PropsWithChildren> = ({ children }) => {
+  const headersList = headers();
+  const prefersColorScheme = headersList.get("sec-ch-prefers-color-scheme");
+
+  return (
+    <html lang={"ru"} data-theme={prefersColorScheme} className={styles.html}>
+      <body className={classNames(styles.body, roboto.variable)}>
+        <Theme />
+        <main className={styles.main}>{children}</main>
+      </body>
+    </html>
+  );
+};
+
 export default RootLayout;
