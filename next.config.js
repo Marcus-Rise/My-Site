ba8866f1b45bef747b92f203045f11cc38792771
Ilya Konstantinov <ilykonst95@gmail.com>
2022-11-10 15:32:10 +0400

    feat: detect theme on server side (#62)

--- next.config.js
+++ next.config.js
@@ -4,4 +4,25 @@ module.exports = {
   experimental: {
     appDir: true,
   },
+  headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Accept-CH",
+            value: "Sec-CH-Prefers-Color-Scheme",
+          },
+          {
+            key: "Vary",
+            value: "Sec-CH-Prefers-Color-Scheme",
+          },
+          {
+            key: "Critical-CH",
+            value: "Sec-CH-Prefers-Color-Scheme",
+          },
+        ],
+      },
+    ];
+  },
 };
