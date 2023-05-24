dc5533035fd49b9d735ac3b51f1bfe38d9338298
Ilya Konstantinov <ilykonst95@gmail.com>
2023-05-25 01:06:46 +0400

    refactor: metadata (#73)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { FC, PropsWithChildren } from "react";
 import styles from "./layout.module.scss";
-import { Roboto } from "@next/font/google";
+import { Roboto } from "next/font/google";
 import classNames from "classnames";
 import "../styles/global.scss";
 import { Theme } from "../components/theme";
-import { cookies, headers } from "next/headers";
-import { THEME_COOKIE_KEY } from "../components/theme/theme-config";
+import type { Metadata } from "next";
+import { description, keywords, title } from "../seo";
+import variables from "../styles/variables.module.scss";
 
 const roboto = Roboto({
   weight: "400",
@@ -14,19 +15,30 @@ const roboto = Roboto({
   display: "swap",
 });
 
-const RootLayout: FC<PropsWithChildren> = ({ children }) => {
-  const userPreferColorScheme = cookies().get(THEME_COOKIE_KEY);
-  const systemDefaultColorScheme = headers().get("sec-ch-prefers-color-scheme");
-  const colorScheme = userPreferColorScheme?.value ?? systemDefaultColorScheme;
+const RootLayout: FC<PropsWithChildren> = ({ children }) => (
+  <html lang={"ru"} className={classNames(styles.html, roboto.variable)}>
+    <body className={styles.body}>
+      <Theme />
+      <main className={styles.main}>{children}</main>
+    </body>
+  </html>
+);
 
-  return (
-    <html lang={"ru"} data-theme={colorScheme} className={styles.html}>
-      <body className={classNames(styles.body, roboto.variable)}>
-        <Theme />
-        <main className={styles.main}>{children}</main>
-      </body>
-    </html>
-  );
+const metadata: Metadata = {
+  title,
+  description,
+  keywords,
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  colorScheme: "light dark",
+  openGraph: {
+    title,
+    description,
+  },
+  themeColor: variables.colorBackgroundDarkest,
 };
 
 export default RootLayout;
+export { metadata };
--- src/seo.ts
+++ src/seo.ts
@@ -2,38 +2,12 @@ import { skills } from "./skills.array";
 
 const title = "Илья Константинов";
 const description = "Ведущий веб-разработчик";
-const keywords: string = [
+const keywords: string[] = [
   ...skills,
-  title,
   ...title.split(" "),
   ...description.split(" "),
   "Самара",
   "Web",
-].join(", ");
-const getSiteMap = (host: string): string =>
-  `
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url>
-<loc>https://${host}/</loc>
-</url>
-</urlset>
-  `;
-const getRobotsTxt = (hostName: string, isAllow: boolean): string => {
-  const robots: Record<string, string> = isAllow
-    ? {
-        "User-agent": "*",
-        Allow: "/",
-        Sitemap: `https://${hostName}/sitemap.xml`,
-      }
-    : {
-        "User-agent": "*",
-        Disallow: "/",
-        Sitemap: `https://${hostName}/sitemap.xml`,
-      };
+];
 
-  return Object.keys(robots)
-    .map((i) => `${i}: ${robots[i]}`)
-    .join("\n");
-};
-
-export { title, description, keywords, getSiteMap, getRobotsTxt };
+export { title, description, keywords };
