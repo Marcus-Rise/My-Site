f09b9e91b861e9a0503b356c72a22996f32e1904
Marcus-Rise <ilykonst95@gmail.com>
2024-02-07 12:06:25 +0400

    fix: suspense for client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { FC, PropsWithChildren } from "react";
+import { Suspense } from "react";
 import { Roboto } from "next/font/google";
 import "./global.css";
 import type { Metadata, Viewport } from "next";
@@ -26,7 +27,9 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => {
           <>
             <Analytics />
             <AnalyticsYandex />
-            <AnalyticsYandexReporter />
+            <Suspense>
+              <AnalyticsYandexReporter />
+            </Suspense>
           </>
         )}
       </body>
